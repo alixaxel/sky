d66f9846c0f28584fcbd8b12ddd1bbaca0285c00
Martin Kobetic <mkobetic@gmail.com>
2014-05-02 16:07:24 +0000

    expunge any remnants of stream-flush-period

--- skyd/config/config_test.go
+++ skyd/config/config_test.go
@@ -9,7 +9,6 @@ const testConfigFileA = `
 port=9000
 data-path="/home/data"
 pid-path = "/home/pid"
-stream-flush-period = 30
 stream-flush-threshold = 500
 newrelic-key = "longinscrutablestringofcharacters"
 `
@@ -27,8 +26,6 @@ func TestDecode(t *testing.T) {
 		t.Fatalf("Invalid data path: %v", config.DataPath)
 	} else if config.PidPath != "/home/pid" {
 		t.Fatalf("Invalid pid path: %v", config.PidPath)
-	} else if config.StreamFlushPeriod != 30 {
-		t.Fatalf("Invalid no stream flush period option: %v", config.StreamFlushPeriod)
 	} else if config.StreamFlushThreshold != 500 {
 		t.Fatalf("Invalid no stream flush threshold option: %v", config.StreamFlushThreshold)
 	} else if config.NewRelicKey != "longinscrutablestringofcharacters" {
